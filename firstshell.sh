5c5f87577ed2477a5675f3ab68cc2c4b291c5283
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-11-13 13:19:14 +0100

    Dealing with user inputs

--- firstshell.sh
+++ firstshell.sh
@@ -7,5 +7,10 @@ echo Hello world!
 # UPPERCASE BY CONVENTION
 # LETTER, NUMBERS, AND UNDERSCORES
 
-NAME="Chisimdi"
-echo "My name is ${NAME}"
+# NAME="Chisimdi"
+# echo "My name is ${NAME}"
+
+# USER INPUTS
+read -p "Enter your Name: " NAME
+read -p "Enter your age: " AGE
+echo "Hello ${NAME}, nice to meet with you. You're ${AGE}years old."
