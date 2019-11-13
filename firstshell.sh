9626c86957bf75fd9e6337b0bdfc271b9a1ffb5c
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-11-13 14:40:51 +0100

    Handling files and folder

--- firstshell.sh
+++ firstshell.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # ECHO COMMAND
-echo Hello world!
+# echo Hello world!
 
 # VARIABLES
 # UPPERCASE BY CONVENTION
@@ -11,9 +11,17 @@ echo Hello world!
 # echo "My name is ${NAME}"
 
 # USER INPUTS
-read -p "Enter your Name: " NAME
-read -p "Enter your age: " AGE
-echo "Hello ${NAME}, nice to meet with you. You're ${AGE}years old."
+# read -p "Enter your Name: " NAME
+# read -p "Enter your age: " AGE
+# echo "Hello ${NAME}, nice to meet with you. You're ${AGE}years old."
+
+# Relational Operators
+# -eq : Equal to
+# -ne : Not equal to
+# -gt : Greater than
+# -lt : Less than
+# -ge : Greater than or equal to
+# -le : Less than or equal to
 
 # SIMPLE IF STATEMENT
 # if [ ${AGE} -ge 28 ]
@@ -30,20 +38,33 @@ echo "Hello ${NAME}, nice to meet with you. You're ${AGE}years old."
 # fi
 
 # ELSE-IF (elif)
-if [ ${AGE} -eq 28 ]
-then
-  echo "${NAME} you're almost old, hope you are planning to get married?."
-elif [ ${AGE} -ge 28 ]
-then
-  echo "${NAME} you're getting old, you need to get married soon."
-else
-  echo "${NAME} you're still young, enjoy your life while it last. :)"
-fi
+# if [ ${AGE} -eq 28 ]
+# then
+#   echo "${NAME} you're almost old, hope you are planning to get married?."
+# elif [ ${AGE} -ge 28 ]
+# then
+#   echo "${NAME} you're getting old, you need to get married soon."
+# else
+#   echo "${NAME} you're still young, enjoy your life while it last. :)"
+# fi
 
-# Relational Operators
-# -eq : Equal to
-# -ne : Not equal to
-# -gt : Greater than
-# -lt : Less than
-# -ge : Greater than or equal to
-# -le : Less than or equal to
+
+#### File Conditionals
+# -d File     True of a file is a directory
+# -e File     True if a file exists (Note that this is not particularly portable, thus -f is generally used)
+# -f File     True if the provided string is a file
+# -g File     True if the group id is set on a file
+# -r File     True if the file is readable
+# -s File     True if the file has a non-zero size
+# -u          True if the user id is set on the file
+# -w          True if the file is writable
+# -x          True if the file is an executable
+####
+
+# FILE="test.txt"
+# if [ -f ${FILE} ]
+# then
+#   echo "${FILE} is a file"
+# else
+#   echo "${FILE} is NOT a file"
+# fi
